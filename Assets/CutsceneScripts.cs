6968b0bb4c159d0454d25fd2ff65f6ffb3ccf81a
Zolotarev Evgenii Vladislavovich <xca0@yandex.ru>
2023-10-02 02:14:07 +0300

    1st scene work

--- Assets/CutsceneScripts.cs
+++ Assets/CutsceneScripts.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using Common.Scripts;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class CutsceneScripts : MonoBehaviour
 {
     [SerializeField] private GameObject Active;
     [SerializeField] private GameObject ActiveText;
+    [SerializeField] private string SceneToLoad;
     public void StopAnim()
     {
         Active.SetActive(false);
@@ -23,5 +25,10 @@ public class CutsceneScripts : MonoBehaviour
     public void Activate()
     {
         ActiveText.SetActive(true);
+    }
+
+    public void LoadingScene()
+    {
+        SceneManager.LoadScene(SceneToLoad);
     }
 }
