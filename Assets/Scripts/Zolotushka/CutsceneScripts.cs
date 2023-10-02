64605793fb56c76691e7800187c3092b8b583b79
Zolotarev Evgenii Vladislavovich <xca0@yandex.ru>
2023-10-02 13:33:16 +0300

    Final scene

---
+++ Assets/Scripts/Zolotushka/CutsceneScripts.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Common.Scripts;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class CutsceneScripts : MonoBehaviour
+{
+    [SerializeField] private GameObject Active;
+    [SerializeField] private GameObject ActiveText;
+    [SerializeField] private string SceneToLoad;
+    [SerializeField] private PlayableDirector _director;
+    public void StopAnim()
+    {
+        Active.SetActive(false);
+    }
+
+    public void RetrieveMovement()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player").GetComponent<movement>();
+        player.enabled = true;
+    }
+
+    public void Activate()
+    {
+        ActiveText.SetActive(true);
+    }
+
+    public void LoadingScene()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+
+    public void DirectorOn()
+    {
+        _director.Play();
+    }
+}
